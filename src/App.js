1cb59f4ebb83c8eae2eec383388ab6bb09a52726
IN22915788 <emil.joshy@tesco.com>
2020-10-03 21:35:17 +0530

    Hands ready

--- src/App.js
+++ src/App.js
@@ -5,10 +5,21 @@ import './App.css';
 function App() {
   return (
     <div className="Clock">
-      <header className="Clock-header">World Clock</header>
-      <div className="ClockFace">
-      
-      </div>    
+      <header className="Clock-header">WORLD CLOCK</header>
+      <div className="ClockContainer">
+        <div className="ClockFace">
+          <div className="Hour">
+            <div className="hr"></div>
+          </div>
+          <div className="Minute">
+            <div className="mn"></div>
+          </div><div className="Second">
+            <div className="sc"></div>
+            </div><div className="Knob">
+            <div className="sc"></div>
+          </div>
+        </div> 
+      </div>   
     </div>
   );
 }
